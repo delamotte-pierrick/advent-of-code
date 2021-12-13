28620ecd596029e7416e9d81ed8cc2992461eb6e
pierrick.delamotte <pierrick.delamotte@gmail.com>
2021-12-13 11:19:24 +0100

    day_one

---
+++ day_1/main.py
@@ -0,0 +1,29 @@
+def day_1(filename):
+    f = open(filename, "r")
+
+    previous = None
+    result = 0
+
+    for x in f:
+        if (previous is not None) and (int(x) > previous):
+            result += 1
+        previous = int(x)
+
+    return result
+
+
+def day_1_2(filename):
+    previous = None
+    result = 0
+
+    with open(filename, "r") as f:
+        dataset = f.read().split('\n')
+
+        for index, x in enumerate(dataset):
+            subset = list(map(int, filter(None, dataset[index:index+3])))
+            elem_sum = sum(subset)
+            if (previous is not None) and (int(elem_sum) > previous):
+                result += 1
+            previous = int(elem_sum)
+
+    return result
---
+++ main.py
@@ -0,0 +1,11 @@
+from day_1.main import day_1
+from day_1.main import day_1_2
+
+if __name__ == '__main__':
+    print(day_1("./data/input_test_1.txt"))
+    print(day_1("./data/input_1.txt"))
+
+    print(day_1_2("./data/input_test_1.txt"))
+    print(day_1_2("./data/input_1.txt"))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
